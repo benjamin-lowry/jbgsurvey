627955ff7846c1859a9d2dd597abbcdd10e3c765
Benjamin Lowry <ben@ben.gmbh>
2024-12-24 17:48:39 -0500

    jbgsurvey: multilingual support, save app config

--- jbgsurvey.py
+++ jbgsurvey.py
@@ -1,8 +1,9 @@
 from requests import get
 from json import load, loads
 from time import sleep
-from os import makedirs
-from os.path import join, exists
+from os import makedirs, utime
+from os.path import basename, join, exists
+from glob import glob
 
 # Constants from BigSurvey.swf
 USER_AGENT = "Mozilla/5.0 (iPad; CPU OS 7_0 like Mac OS X) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11A465 Safari/9537.53"
@@ -17,9 +18,8 @@ NUM_KEY = "f"
 SYNONYMS_KEY = "a"
 EXPLICIT_KEY = "x"
 
-# List of languages for which we need to chop off the country code when looking
-# up onlyForLobbyContentIds
-LANGUAGE_MAP = {"de-DE": "de", "es-ES": "es", "fr-FR": "fr", "it-IT": "it"}
+# Country codes to add to language tags without country
+LANG_COUNTRIES = {"en": ["en-AU", "en-GB", "en-US"]}
 
 class BigsurveyResponse():
     def __init__(self, word, num, synonyms, explicit):
@@ -54,42 +54,52 @@ def get_responses(question_id, language, base_url, filename):
     else:
         url = base_url + "/" + str(question_id) + "/" + filename
         resp = get(url, headers={"user-agent":USER_AGENT})
-        with open(destfile, "wb") as f:
-            f.write(resp.content)
-        return parse_survey_dict(resp.json())
+        if resp.status_code == 200:
+            with open(destfile, "wb") as f:
+                f.write(resp.content)
+            timestamp = int(filename.replace(".json", "").split("-")[-1])
+            utime(destfile, (timestamp, timestamp))
+            return parse_survey_dict(resp.json())
+        else:
+            return []
 
 def get_app_config(server, protocol, game_tag):
-    return get(protocol + "://" + server + "/api/v2/app-configs/" + game_tag,
-               headers={"user-agent":USER_AGENT}).json()
-
+    resp = get(protocol + "://" + server + "/api/v2/app-configs/" + game_tag,
+               headers={"user-agent":USER_AGENT})
+    json = resp.json()
+    # Find the highest surveyDataFilename timestamp,
+    # to name the app-config file
+    timestamp = 0
+    for key, value in json["body"]["settings"].items():
+        if key.startswith("surveyDataFilename_"):
+            newstamp = int(value.replace(".json", "").split("-")[-1])
+            if newstamp > timestamp: timestamp = newstamp
+    makedirs("app-configs", exist_ok=True)
+    dest = join("app-configs", "%s.json" % timestamp)
+    with open(dest, "wb") as f:
+        f.write(resp.content)
+    utime(dest, (timestamp, timestamp))
+    return json
 if __name__ == "__main__":
     app_config = get_app_config(SERVER, PROTOCOL, GAME_TAG)
-    all_languages = []
-    for key in app_config["body"]["settings"].keys():
-        if key.startswith("surveyDataFilename_"):
-            all_languages.append(key.replace("surveyDataFilename_", ""))
-    scrape_languages = all_languages
-    with open("content_ids.txt", "r") as f:
-        f.seek(0, 2)
-        size = f.tell()
-        f.seek(0)
-        content_id = f.readline().strip()
-        while content_id:
-            print("\r" + "%.2f" % round(f.tell() / size * 100, 2) + "%", end="")
-            for language in scrape_languages:
-                if language.startswith("en-"):
-                    content_ban_id = content_id
-                else:
-                    if language.find("-") == -1:
-                        content_ban_id = content_id + "_" + language + "-" + language.upper()
-                    else:
-                        content_ban_id = content_id + "_" + language
-                if not content_ban_id in app_config["body"]["settings"]["onlyForLobbyContentIds"]:
-                    try:
-                        responses = get_responses(content_id, language,
-                          app_config["body"]["settings"]["surveyDataBaseURL"],
-                          app_config["body"]["settings"]["surveyDataFilename_" + language])
-                    except Exception as e:
-                        print("error on content", content_id, "language", language, e)
+    for language_contents_filename in glob("content_ids/*.txt"):
+        language = basename(language_contents_filename).replace(".txt", "")
+        with open(language_contents_filename, "r") as f:
+            f.seek(0, 2)
+            size = f.tell()
+            f.seek(0)
             content_id = f.readline().strip()
+            while content_id:
+                print("\r" + language + " %.2f" % round(f.tell() / size * 100, 2) + "%", end="")
+                if not content_id in app_config["body"]["settings"]["onlyForLobbyContentIds"]:
+                    for lang_country in (LANG_COUNTRIES[language] if language
+                                         in LANG_COUNTRIES else [language]):
+                        try:
+                            responses = get_responses(content_id.split("_")[0].replace("TR-", ""), language,
+                              app_config["body"]["settings"]["surveyDataBaseURL"],
+                              app_config["body"]["settings"]["surveyDataFilename_" + lang_country])
+                        except Exception as e:
+                            print("error on content", content_id, "language", language, e)
+                content_id = f.readline().strip()
+        print()
     print()
